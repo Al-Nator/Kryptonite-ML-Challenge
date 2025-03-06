983cd9e40ed113870efc108416198c42a22211d9
kekwak <kekwak@mail.ru>
2025-03-07 01:44:26 +0300

    New files && Minor fixes

--- aligner/wrapper.py
+++ aligner/wrapper.py
@@ -7,21 +7,21 @@ import yaml
 class ModelConfig(PretrainedConfig):
 
     def __init__(
-            self,
+            self, path='aligner/pretrained_model/model.yaml',
             **kwargs,
     ):
         super().__init__(**kwargs)
-        self.conf = dict(yaml.safe_load(open('aligner/pretrained_model/model.yaml')))
+        self.conf = dict(yaml.safe_load(open(path)))
 
 
 class CVLFaceAlignmentModel(PreTrainedModel):
     config_class = ModelConfig
 
-    def __init__(self, cfg):
+    def __init__(self, cfg, path='aligner/pretrained_model/model.pt'):
         super().__init__(cfg)
         model_conf = OmegaConf.create(cfg.conf)
         self.model = get_aligner(model_conf)
-        self.model.load_state_dict_from_path('aligner/pretrained_model/model.pt')
+        self.model.load_state_dict_from_path(path)
 
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
